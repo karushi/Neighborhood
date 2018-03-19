c4436349c166eb48b68875bf3c55a5b3b72315f9
karushi <you@example.com>
2018-03-19 15:54:42 +0300

    create models

--- neighborhood/neighbor/models.py
+++ neighborhood/neighbor/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 # Create your models here.
 
-User=get_user_model()
+# User=get_user_model()
 
 class Neighbourhood(models.Model):
     name=models.CharField(max_length=80)
@@ -13,6 +15,24 @@ class Neighbourhood(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def create_neigborhood(self):
+            self.save()
+
+    def delete_neigborhood(self):
+            self.delete()
+
+    @classmethod
+    def find_neigborhood(cls,neigborhood_id):
+            pass
+
+    @classmethod
+    def update_neighborhood(cls):
+            pass
+
+    @classmethod
+    def update_occupants(cls):
+            pass    
 
 class Business(models.Model):
     name=models.CharField(max_length=60)
@@ -24,18 +44,34 @@ class Business(models.Model):
     def __str__(self):
         return self.name
 
+    def create_business(self):
+            self.save()
+
+    def delete_business(self):
+            self.delete()
+
+    @classmethod
+    def find_business(cls,business_id):
+            pass
+
+    @classmethod
+    def update_business(cls):
+            pass    
+
 
         
 class User(models.Model):
     name=models.CharField(max_length=60)
     profile_phots=models.ImageField(upload_to='images/',blank=True)
-    user_id=models.IntegerField()
+    user_id=models.ForeignKey(User,on_delete=models.CASCADE)
     Neighbour=models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
     user_email=models.CharField(max_length=60)
 
 
     def __str__(self):
         return self.name
+
+        
 
 
 class Post(models.Model):
@@ -52,3 +88,15 @@ class Post(models.Model):
     @classmethod
     def get_post(cls):
         posts=Post.objects.all()
+
+
+    @classmethod
+    def search_by_business(cls,search_term):    
+        profile=cls.objects.filter(user__username__icontains=search_term)
+        return profile  
+
+# def create_user(sender, **kwargs):
+#      if kwargs['created']:
+#            profile = Profile.objects.create(user_id=kwargs['instance'])
+
+# post_save.connect(create_user, sender=User)
